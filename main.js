67f227e1c2f96ce50106351f1a4cb2565b0d6d5e
azaharii <azah6428@gmail.com>
2026-01-19 09:05:56 +0700

    initialize firebase and import firestore function

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+// Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js"
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js"
